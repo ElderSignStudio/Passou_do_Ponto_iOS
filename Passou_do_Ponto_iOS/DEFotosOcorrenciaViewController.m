2e351bf4986575f1207de5ec30bb5dd9831d551e
Dan Eisenberg <daneisuk@gmail.com>
2015-08-11 00:02:44 +0100

    Retirei alguns comments sobrando.

--- Passou_do_Ponto_iOS/DEFotosOcorrenciaViewController.m
+++ Passou_do_Ponto_iOS/DEFotosOcorrenciaViewController.m
@@ -124,16 +124,6 @@
 
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 #pragma mark - Buttons
 
 - (IBAction)photoButtonPressed:(UIButton *)sender {
@@ -200,8 +190,6 @@
         
         [strongDelegate photosChosen:(NSArray *)self.photoMutableArray];
     }
-    
-    //[self.photoMutableArray removeAllObjects];
     
 }
 
